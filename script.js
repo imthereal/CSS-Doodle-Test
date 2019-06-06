6615fda091bbc384abf727d3248c2f6ad205411c
Audrey McNay <audreymcnay@Audreys-MacBook-Pro.local>
2019-06-05 23:38:35 -0700

    fixed browser viewport height issues

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener("click", e => e.target.update());
-//why the heck does this not work with any other object calling the addeventlistener method.... gotta read up on js
 /*
 document.addEventListener('click', function(e) {
   e.target.update && e.target.update();
